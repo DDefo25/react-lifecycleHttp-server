b3c8b7e61573ef5db8537deb0ca8561eec8a356a
ddefo25 <ispumamm@gmail.com>
2023-06-03 21:42:43 +0200

    merge

--- server.js
+++ server.js
@@ -13,32 +13,55 @@ app.use(
   })
 );
 app.use(function (req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Content-Type", "application/json");
   next();
 });
 
-const notes = [];
+let posts = [];
 let nextId = 1;
 
-app.get("/notes", (req, res) => {
-  res.send(JSON.stringify(notes));
+app.get("/posts", (req, res) => {
+  res.send(JSON.stringify(posts));
 });
 
-app.post("/notes", (req, res) => {
-  notes.push({ ...req.body, id: nextId++ });
+app.get("/posts/:id", (req, res) => {
+  const postId = Number(req.params.id);
+  const index = posts.findIndex((o) => o.id === postId);
+  res.send(JSON.stringify({ post: posts[index] }));
+});
+
+app.post("/posts", (req, res) => {
+  posts.push({ ...req.body, id: nextId++, created: Date.now() });
   res.status(204);
   res.end();
 });
 
-app.delete("/notes/:id", (req, res) => {
-  const noteId = Number(req.params.id);
-  const index = notes.findIndex((o) => o.id === noteId);
+app.put("/posts/:id", (req, res) => {
+  const postId = Number(req.params.id);
+  posts = posts.map((o) => {
+    if (o.id === postId) {
+      return {
+        ...o,
+        ...req.body,
+        id: o.id,
+      };
+    }
+    return o;
+  });
+  res.status(204).end();
+});
+
+app.delete("/posts/:id", (req, res) => {
+  const postId = Number(req.params.id);
+  const index = posts.findIndex((o) => o.id === postId);
   if (index !== -1) {
-    notes.splice(index, 1);
+    posts.splice(index, 1);
   }
   res.status(204);
   res.end();
 });
 
 const port = process.env.PORT || 7070;
-app.listen(port, () => console.log(`The server is running on http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`The server is running on http://localhost:${port}`)
+);
